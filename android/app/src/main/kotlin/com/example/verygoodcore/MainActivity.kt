e643012b92a8eac46469ec02ee39d93f0c4a1bcb
Stefan <shodgeskluck@gmail.com>
2024-01-29 20:45:45 -0500

    initial commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.personal_blog_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:personal_blog_flutter/app/app.dart';
+import 'package:personal_blog_flutter/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
