0a0c79970361d08d5c050921cea315162a6fc407
Stefan <shodgeskluck@gmail.com>
2024-02-03 15:14:02 -0500

    test wip

--- packages/blog_ui/test/src/widgets/author_avatar_test.dart
+++ packages/blog_ui/test/src/widgets/author_avatar_test.dart
@@ -1,5 +1,19 @@
+import 'package:blog_ui/blog_ui.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+import '../../helpers/pumps.dart';
+
 void main() {
-  group('AuthorAvatar', () {});
+  group('AuthorAvatar', () {
+    testWidgets('renders', (tester) async {
+      await tester.pumpMaterial(
+        const AuthorAvatar(
+          imageUrl: 'imageUrl',
+        ),
+        mockNetworkImages: true,
+      );
+
+      expect(find.byType(AuthorAvatar), findsOneWidget);
+    });
+  });
 }
--- packages/blog_ui/test/src/widgets/author_tile_test.dart
+++ packages/blog_ui/test/src/widgets/author_tile_test.dart
@@ -1,5 +1,28 @@
+import 'package:blog_ui/src/widgets/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+import '../../helpers/pump_material.dart';
+
 void main() {
-  group('AuthorTile', () {});
+  group('AuthorTile', () {
+    testWidgets('renders without image', (tester) async {
+      await tester.pumpMaterial(const AuthorTile(author: 'author'));
+
+      expect(find.byType(AuthorTile), findsOneWidget);
+      expect(find.byType(AuthorAvatar), findsNothing);
+    });
+
+    testWidgets('renders with image', (tester) async {
+      await tester.pumpMaterial(
+        const AuthorTile(
+          author: 'author',
+          authorImage: 'authorImage',
+        ),
+        mockNetworkImages: true,
+      );
+
+      expect(find.byType(AuthorTile), findsOneWidget);
+      expect(find.byType(AuthorAvatar), findsOneWidget);
+    });
+  });
 }
