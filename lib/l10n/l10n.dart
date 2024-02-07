9373a9cdcf6cfc5b93556d84e36eca8633ffcae0
Stefan <shodgeskluck@gmail.com>
2024-02-07 18:53:01 -0500

    ok, let's try on the file...

--- lib/l10n/l10n.dart
+++ lib/l10n/l10n.dart
@@ -1,8 +1,9 @@
+// coverage:ignore-file
 import 'package:flutter/widgets.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 export 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 extension AppLocalizationsX on BuildContext {
-  AppLocalizations get l10n => AppLocalizations.of(this); //coverage:ignore-line
+  AppLocalizations get l10n => AppLocalizations.of(this);
 }
