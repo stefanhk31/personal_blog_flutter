24f3f8080ca2e0ae589e229f34d7b58d70f90361
Stefan <shodgeskluck@gmail.com>
2024-02-08 10:38:19 -0500

    test updates

--- packages/blog_ui/test/src/theme/blog_theme_test.dart
+++ packages/blog_ui/test/src/theme/blog_theme_test.dart
@@ -4,22 +4,44 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   group('BlogTheme', () {
-    test('uses material 3', () {
-      expect(BlogTheme.lightThemeData.useMaterial3, isTrue);
+    group('LightThemeData', () {
+      test('uses material 3', () {
+        expect(BlogTheme.lightThemeData.useMaterial3, isTrue);
+      });
+
+      test('background color is seedLightBackground', () {
+        expect(
+          BlogTheme.lightThemeData.colorScheme.background,
+          equals(BlogColors.seedLightBackground),
+        );
+      });
+
+      test('visual density is adaptive platform density', () {
+        expect(
+          BlogTheme.lightThemeData.visualDensity,
+          equals(VisualDensity.adaptivePlatformDensity),
+        );
+      });
     });
 
-    test('background color is seedWhite', () {
-      expect(
-        BlogTheme.lightThemeData.colorScheme.background,
-        equals(BlogColors.seedLightBackground),
-      );
-    });
+    group('DarkThemeData', () {
+      test('uses material 3', () {
+        expect(BlogTheme.darkThemeData.useMaterial3, isTrue);
+      });
 
-    test('visual density is adaptive platform density', () {
-      expect(
-        BlogTheme.lightThemeData.visualDensity,
-        equals(VisualDensity.adaptivePlatformDensity),
-      );
+      test('background color is seedDarkBackground', () {
+        expect(
+          BlogTheme.darkThemeData.colorScheme.background,
+          equals(BlogColors.seedDarkBackground),
+        );
+      });
+
+      test('visual density is adaptive platform density', () {
+        expect(
+          BlogTheme.darkThemeData.visualDensity,
+          equals(VisualDensity.adaptivePlatformDensity),
+        );
+      });
     });
   });
 }
