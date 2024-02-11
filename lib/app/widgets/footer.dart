705e06a97bd5bd3a24cf78ddfffd341cb1774d39
Stefan Hodges-Kluck <42074868+stefanhk31@users.noreply.github.com>
2024-02-11 12:47:25 -0500

    fix: footer renderflex overflow (#35)

--- lib/app/widgets/footer.dart
+++ lib/app/widgets/footer.dart
@@ -22,11 +22,19 @@ class Footer extends StatelessWidget {
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Image.asset(imageSource),
-            Text(
-              context.l10n.copyrightText(DateTime.now().year),
-              style: BlogTextStyles.footerTextStyle.copyWith(
-                color: Theme.of(context).colorScheme.onPrimary,
+            Expanded(
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Image.asset(imageSource),
+              ),
+            ),
+            Expanded(
+              child: Text(
+                context.l10n.copyrightText(DateTime.now().year),
+                style: BlogTextStyles.footerTextStyle.copyWith(
+                  color: Theme.of(context).colorScheme.onPrimary,
+                ),
+                textAlign: TextAlign.end,
               ),
             ),
           ],
