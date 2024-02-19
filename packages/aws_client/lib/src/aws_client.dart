64bfe6281a3df2ba07a50a39beb16e6dd7181e3e
Stefan <shodgeskluck@gmail.com>
2024-02-19 10:32:53 -0500

    adding decryption

--- packages/aws_client/lib/src/aws_client.dart
+++ packages/aws_client/lib/src/aws_client.dart
@@ -1,7 +1,26 @@
+import 'dart:convert';
+
+import 'package:encrypt/encrypt.dart';
+
 /// {@template aws_client}
 /// A Very Good Project created by Very Good CLI.
 /// {@endtemplate}
 class AwsClient {
   /// {@macro aws_client}
   const AwsClient();
+
+  String decrypt({
+    required String secret,
+    required String encryptionKey,
+    required String initializationVector,
+  }) {
+    final key = Key.fromBase64(encryptionKey);
+    final iv = IV.fromBase64(initializationVector);
+    final encrypter = Encrypter(AES(key, mode: AESMode.cbc));
+
+    final encryptedBytes = base64.decode(secret);
+    final decryptedBytes =
+        encrypter.decryptBytes(Encrypted(encryptedBytes), iv: iv);
+    return utf8.decode(decryptedBytes);
+  }
 }
