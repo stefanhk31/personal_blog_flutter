64d0a0a0cb27c4df4c6a95531a2e7cb2e304fb64
Stefan <shodgeskluck@gmail.com>
2024-02-27 14:30:22 -0500

    removing local debug code

--- api/routes/_middleware.dart
+++ api/routes/_middleware.dart
@@ -4,14 +4,11 @@ import 'package:butter_cms_client/butter_cms_client.dart';
 import 'package:dart_frog/dart_frog.dart';
 import 'package:http/http.dart';
 
-import '../secrets.dart';
-
 Handler middleware(Handler handler) {
   return handler.use(requestLogger()).use(
     provider<ButterCmsClient>(
       (_) {
-        final apiKey =
-            Platform.environment['butter_cms_api_key'] ?? butterCmsApiKey;
+        final apiKey = Platform.environment['butter_cms_api_key'];
 
         return ButterCmsClient(
           httpClient: Client(),
