ae4d8bea36e99be428378daea156eaf71b2a0eb6
Stefan <shodgeskluck@gmail.com>
2024-03-02 10:17:43 -0500

    http client wrapper

--- packages/blog_api_client/lib/src/blog_api_client.dart
+++ packages/blog_api_client/lib/src/blog_api_client.dart
@@ -1,7 +1,32 @@
+import 'dart:io';
+
+import 'package:http/http.dart';
+
 /// {@template blog_api_client}
-/// A Very Good Project created by Very Good CLI.
+/// An implementation of an HTTP client to interact with the blog API.
 /// {@endtemplate}
-class BlogApiClient {
+class BlogApiClient extends BaseClient {
   /// {@macro blog_api_client}
-  const BlogApiClient();
+  BlogApiClient({required Client innerClient}) : _innerClient = innerClient;
+
+  final Client _innerClient;
+
+  @override
+  Future<StreamedResponse> send(BaseRequest request) {
+    request.headers.putIfAbsent(
+      HttpHeaders.contentTypeHeader,
+      () => ContentType.json.value,
+    );
+    request.headers.putIfAbsent(
+      HttpHeaders.acceptHeader,
+      () => ContentType.json.value,
+    );
+    return _innerClient.send(request);
+  }
+
+  @override
+  void close() {
+    _innerClient.close();
+    super.close();
+  }
 }
--- packages/blog_api_client/test/src/blog_api_client_test.dart
+++ packages/blog_api_client/test/src/blog_api_client_test.dart
@@ -1,11 +1,72 @@
 // ignore_for_file: prefer_const_constructors
-import 'package:test/test.dart';
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:blog_api_client/blog_api_client.dart';
+import 'package:http/http.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:test/test.dart';
+
+class _MockHttpClient extends Mock implements Client {}
+
+class _MockRequest extends Mock implements Request {}
 
 void main() {
   group('BlogApiClient', () {
+    late Client httpClient;
+    late BlogApiClient blogApiClient;
+    late Request request;
+
+    setUpAll(() {
+      registerFallbackValue(Request('GET', Uri()));
+    });
+
+    setUp(
+      () {
+        httpClient = _MockHttpClient();
+        blogApiClient = BlogApiClient(
+          innerClient: httpClient,
+        );
+
+        when(() => httpClient.send(any())).thenAnswer(
+          (_) async => StreamedResponse(
+            Stream.value(utf8.encode('')),
+            HttpStatus.ok,
+          ),
+        );
+        request = _MockRequest();
+        when(() => request.headers).thenReturn({});
+      },
+    );
+
     test('can be instantiated', () {
-      expect(BlogApiClient(), isNotNull);
+      expect(
+        BlogApiClient,
+        isNotNull,
+      );
+    });
+
+    group('send', () {
+      test('adds content type and accept headers', () async {
+        await blogApiClient.send(request);
+        expect(
+          request.headers,
+          equals(
+            {
+              HttpHeaders.contentTypeHeader: ContentType.json.value,
+              HttpHeaders.acceptHeader: ContentType.json.value,
+            },
+          ),
+        );
+      });
+    });
+
+    group('close', () {
+      test('closes inner client on close', () {
+        when(() => httpClient.close()).thenAnswer((_) {});
+        blogApiClient.close();
+        verify(() => httpClient.close()).called(1);
+      });
     });
   });
 }
