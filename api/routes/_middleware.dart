a2d8388b27dfd12e90706a50b7f8378cde5244e6
Stefan Hodges-Kluck <42074868+stefanhk31@users.noreply.github.com>
2024-03-03 10:20:48 -0500

    feat: add cors (#51)

--- api/routes/_middleware.dart
+++ api/routes/_middleware.dart
@@ -3,18 +3,28 @@ import 'dart:io';
 import 'package:butter_cms_client/butter_cms_client.dart';
 import 'package:dart_frog/dart_frog.dart';
 import 'package:http/http.dart';
+import 'package:shelf_cors_headers/shelf_cors_headers.dart';
 
 Handler middleware(Handler handler) {
   return handler.use(requestLogger()).use(
     provider<ButterCmsClient>(
       (_) {
-        final apiKey = Platform.environment['butter_cms_api_key'];
+        final apiKey = Platform.environment['BUTTER_CMS_API_KEY'];
 
         return ButterCmsClient(
           httpClient: Client(),
           apiKey: apiKey ?? '',
         );
       },
+    ),
+  ).use(
+    fromShelfMiddleware(
+      corsHeaders(
+        headers: {
+          ACCESS_CONTROL_ALLOW_ORIGIN:
+              Platform.environment['CORS_ALLOW_ORIGIN'] ?? '*',
+        },
+      ),
     ),
   );
 }
