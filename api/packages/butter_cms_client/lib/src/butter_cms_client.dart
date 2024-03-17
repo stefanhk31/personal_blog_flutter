fa501d4f2dd4c6abc4c8be8beae5aa660c1a43f1
Stefan <shodgeskluck@gmail.com>
2024-03-17 18:44:52 -0400

    debug test #9

--- api/packages/butter_cms_client/lib/src/butter_cms_client.dart
+++ api/packages/butter_cms_client/lib/src/butter_cms_client.dart
@@ -25,8 +25,10 @@ class ButterCmsClient {
     bool excludeBody = false,
   }) async {
     var apiKey = Platform.environment['BUTTER_CMS_API_KEY'];
+
     if (apiKey != null) {
-      apiKey = jsonDecode(apiKey) as String;
+      final apiKeyMap = jsonDecode(apiKey) as Map<String, dynamic>;
+      apiKey = apiKeyMap['BUTTER_CMS_API_KEY'] as String;
     }
     final queryParameters = <String, dynamic>{
       'auth_token': apiKey,
