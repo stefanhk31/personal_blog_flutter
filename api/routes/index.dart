9de4a5848605a55d3d792d4df492cd9e4a63ddab
Stefan <shodgeskluck@gmail.com>
2024-03-17 15:31:32 -0400

    remove debug test

--- api/routes/index.dart
+++ api/routes/index.dart
@@ -1,9 +1,6 @@
-import 'dart:io';
-
 import 'package:dart_frog/dart_frog.dart';
 
 /// Defaul request handler.
 Response onRequest(RequestContext context) {
-  final apiKey = Platform.environment['BUTTER_CMS_API_KEY'];
-  return Response(body: 'Welcome to Dart Frog! $apiKey');
+  return Response(body: 'Welcome to Dart Frog!');
 }
