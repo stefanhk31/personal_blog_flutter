e78702df52aeea30fb3ffaa515535b1dc2d88914
Stefan <stefan@verygood.ventures>
2024-04-05 20:21:38 -0400

    scaffold for blog detail widget

---
+++ packages/blog_ui/lib/src/widgets/blog_detail_content.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+/// {@template blog_detail_content}
+/// A widget that displays detailed content of a blog post.
+/// {@endtemplate}
+class BlogDetailContent extends StatelessWidget {
+  /// {@macro blog_detail_content}
+  const BlogDetailContent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- packages/blog_ui/lib/src/widgets/widgets.dart
+++ packages/blog_ui/lib/src/widgets/widgets.dart
@@ -1,4 +1,5 @@
 export 'author_avatar.dart';
 export 'author_tile.dart';
 export 'blog_card.dart';
+export 'blog_detail_content.dart';
 export 'featured_image.dart';
