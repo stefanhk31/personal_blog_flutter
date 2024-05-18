ea861282b7addded8736b08f6a2b11c276462703
Stefan <shodgeskluck@gmail.com>
2024-05-18 14:30:04 -0400

    WIP adding url testing

--- lib/utils/url_launcher.dart
+++ lib/utils/url_launcher.dart
@@ -1,7 +1,6 @@
-// Wrapper around url_launcher package to allow for mocking and testing.
-// coverage:ignore-file
 import 'package:url_launcher/url_launcher_string.dart';
 
+/// Wrapper around url_launcher package to allow for mocking and testing.
 class UrlLauncher {
   Future<bool> validateUrl({required String url}) async =>
       canLaunchUrlString(url);
