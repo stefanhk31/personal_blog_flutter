05f69917e55c46e5458f5d585be65dddc51fe70a
Stefan <shodgeskluck@gmail.com>
2024-07-03 19:21:12 -0400

    AWS SES client package

---
+++ api/packages/aws_ses_client/lib/src/aws_ses_client.dart
@@ -0,0 +1,7 @@
+/// {@template aws_ses_client}
+/// A client for interacting with AWS' Simple Email Service (SES).
+/// {@endtemplate}
+class AwsSesClient {
+  /// {@macro aws_ses_client}
+  const AwsSesClient();
+}
