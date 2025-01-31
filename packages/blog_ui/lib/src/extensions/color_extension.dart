c078cf2566187df1b2db7bb0f80e279882cdcdc3
Stefan <stefan@verygood.ventures>
2025-01-31 11:46:30 -0500

    ignoring dep'd member since I'm moving away from Flutter front-end

--- packages/blog_ui/lib/src/extensions/color_extension.dart
+++ packages/blog_ui/lib/src/extensions/color_extension.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 extension ColorExtension on Color {
   /// Parses the hex string from a [Color].
   String toHex() {
+    // ignore: deprecated_member_use
     return '#${value.toRadixString(16).substring(2)}';
   }
 }
