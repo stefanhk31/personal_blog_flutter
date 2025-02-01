534c2ffd2987aeea2343b2749f45caa24908a428
Stefan <shodgeskluck@gmail.com>
2025-02-01 17:05:12 -0500

    wip theme

--- api/tailwind.config.js
+++ api/tailwind.config.js
@@ -2,8 +2,27 @@
 module.exports = {
   content: ["**/*.{dart,html}"],
   theme: {
-    extend: {},
-  },
+    extend: {
+      colors: {
+        primary: {
+          light: '#9C6BA6',
+          dark: '#6F4180',
+        },
+        secondary: {
+          light: '#D8B3E6',
+          dark: '#4D4D4D',
+        },
+        background: {
+          light: '#CFCFCF',
+          dark: '#333233',
+        },
+        text: {
+          light: '#333233',
+          dark: '#CFCFCF',
+        },
+      },
+    },
+  },  
   plugins: [
     require('@tailwindcss/typography'),
   ],
