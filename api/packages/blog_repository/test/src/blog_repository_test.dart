2ba883413cb8d176722fd8cdc66409122d427eb3
Stefan <shodgeskluck@gmail.com>
2025-02-17 10:02:47 -0500

    tests

--- api/packages/blog_repository/test/src/blog_repository_test.dart
+++ api/packages/blog_repository/test/src/blog_repository_test.dart
@@ -1,7 +1,11 @@
 // ignore_for_file: prefer_const_constructors
+import 'dart:convert';
+
 import 'package:blog_html_builder/blog_html_builder.dart';
+import 'package:blog_models/blog_models.dart';
 import 'package:blog_repository/blog_repository.dart';
 import 'package:butter_cms_client/butter_cms_client.dart';
+import 'package:http/http.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:test/test.dart';
 
@@ -26,6 +30,100 @@ void main() {
       expect(blogRepository, isNotNull);
     });
 
-    group('getBlogDetailHtml', () {});
+    group('getBlogDetailHtml', () {
+      test(
+          'uses template engine to render blog detail page '
+          'when api call is successful', () async {
+        when(() => cmsClient.fetchBlogPost(slug: any(named: 'slug')))
+            .thenAnswer(
+          (_) async => Response(
+            jsonEncode({
+              'meta': blogMetaJson,
+              'data': blogJson,
+            }),
+            200,
+          ),
+        );
+
+        when(
+          () => templateEngine.render(
+            filePath: 'blog_detail_page.html',
+            context: any(named: 'context'),
+          ),
+        ).thenAnswer((_) async => '<html></html>');
+
+        await blogRepository.getBlogDetailHtml('my-blog-post');
+        verify(
+          () => templateEngine.render(
+            filePath: 'blog_detail_page.html',
+            context: any(named: 'context'),
+          ),
+        ).called(1);
+      });
+
+      test(
+          'uses template engine to render error page '
+          'when api call is not successful', () async {
+        when(() => cmsClient.fetchBlogPost(slug: any(named: 'slug')))
+            .thenAnswer(
+          (_) async => Response(
+            'bad request',
+            400,
+          ),
+        );
+
+        when(
+          () => templateEngine.render(
+            filePath: 'error_page.html',
+            context: any(named: 'context'),
+          ),
+        ).thenAnswer((_) async => '<html></html>');
+
+        await blogRepository.getBlogDetailHtml('my-blog-post');
+        verify(
+          () => templateEngine.render(
+            filePath: 'error_page.html',
+            context: any(named: 'context'),
+          ),
+        ).called(1);
+      });
+
+      test(
+          'uses template engine to render error page '
+          'when an unexpected error occurs', () async {
+        when(() => cmsClient.fetchBlogPost(slug: any(named: 'slug')))
+            .thenAnswer(
+          (_) async => Response(
+            jsonEncode({
+              'meta': blogMetaJson,
+              'data': blogJson,
+            }),
+            200,
+          ),
+        );
+
+        when(
+          () => templateEngine.render(
+            filePath: 'blog_detail_page.html',
+            context: any(named: 'context'),
+          ),
+        ).thenThrow(Exception('error rendering template'));
+
+        when(
+          () => templateEngine.render(
+            filePath: 'error_page.html',
+            context: any(named: 'context'),
+          ),
+        ).thenAnswer((_) async => '<html></html>');
+
+        await blogRepository.getBlogDetailHtml('my-blog-post');
+        verify(
+          () => templateEngine.render(
+            filePath: 'error_page.html',
+            context: any(named: 'context'),
+          ),
+        ).called(1);
+      });
+    });
   });
 }
